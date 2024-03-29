e19acde1847c35bd9e025b02847900a257fb1988
andrey.chunov <andrey.chunov@tele2.ru>
2024-03-29 11:23:46 +0300

    edit project

--- src/main/java/ru/praktikum/services/pages/HomePage.java
+++ src/main/java/ru/praktikum/services/pages/HomePage.java
@@ -8,21 +8,43 @@ import org.openqa.selenium.WebElement;
 public class HomePage {
     private WebDriver driver;
 
-    private By tableItems = By.xpath("//div[@class='accordion']");
+    private By questionField;
+
+    private By answerField;
+
     private By coockie = By.xpath("//button[@id='rcc-confirm-button']");
+
+    private By tableItems = By.xpath("//div[@class='accordion']");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public WebElement getQuestionField() {
+        return driver.findElement(questionField);
+    }
+
+    public void setQuestionField(String question) {
+        this.questionField = By.xpath("//div[@class='accordion']//div//div[text()='" + question + "']");
+
+    }
+
+    public By getAnswerField() {
+        return answerField;
+    }
+
+    public void setAnswerField(String answer) {
+        this.answerField = By.xpath("//div[@class='accordion']//div//p[text()='" + answer + "']");
     }
 
     public WebElement getTableItems() {
         return driver.findElement(tableItems);
     }
 
+
     public void clickCoockie() {
         driver.findElement(coockie).click();
     }
-
 
 
 }
--- src/test/java/ru/praktikum/services/DropDownListTest.java
+++ src/test/java/ru/praktikum/services/DropDownListTest.java
@@ -2,44 +2,57 @@ package ru.praktikum.services;
 
 
 import org.junit.Test;
-import org.openqa.selenium.By;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.praktikum.services.pages.HomePage;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class DropDownListTest extends BaseTests {
 
+    private String question;
+    private String answer;
+
+    public DropDownListTest(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"Сколько это стоит? И как оплатить?", "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {"Хочу сразу несколько самокатов! Так можно?", "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {"Как рассчитывается время аренды?", "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {"Можно ли заказать самокат прямо на сегодня?", "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {"Можно ли продлить заказ или вернуть самокат раньше?", "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                {"Вы привозите зарядку вместе с самокатом?", "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                {"Можно ли отменить заказ?", "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                {"Я жизу за МКАДом, привезёте?", "Да, обязательно. Всем самокатов! И Москве, и Московской области."}
+        };
+    }
+
     @Test
-    public void checkResult() throws InterruptedException {
+    public void checkResult() {
         driver.get("https://qa-scooter.praktikum-services.ru/");
-        Map<String, String> questionsAndAnswers = new HashMap<>();
-        questionsAndAnswers.put("Сколько это стоит? И как оплатить?", "Сутки — 400 рублей. Оплата курьеру — наличными или картой.");
-        questionsAndAnswers.put("Хочу сразу несколько самокатов! Так можно?", "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.");
-        questionsAndAnswers.put("Как рассчитывается время аренды?", "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.");
-        questionsAndAnswers.put("Можно ли заказать самокат прямо на сегодня?", "Только начиная с завтрашнего дня. Но скоро станем расторопнее.");
-        questionsAndAnswers.put("Можно ли продлить заказ или вернуть самокат раньше?", "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.");
-        questionsAndAnswers.put("Вы привозите зарядку вместе с самокатом?", "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.");
-        questionsAndAnswers.put("Можно ли отменить заказ?", "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.");
-        questionsAndAnswers.put("Я жизу за МКАДом, привезёте?", "Да, обязательно. Всем самокатов! И Москве, и Московской области.");
         HomePage homePage = new HomePage(driver);
         WebElement element = homePage.getTableItems();
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
         homePage.clickCoockie();
+        homePage.setQuestionField(question);
+        WebElement question = homePage.getQuestionField();
+        question.click();
+        homePage.setAnswerField(answer);
+        WebElement answerField = driver.findElement(homePage.getAnswerField());
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        List<WebElement> questionElement = homePage.getTableItems().findElements(By.xpath(".//div//div//div"));
-        List<WebElement> answerElement = homePage.getTableItems().findElements(By.xpath(".//div//div//p"));
-        for (int i = 0; i < questionElement.size(); i++) {
-            questionElement.get(i).click();
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='accordion']//div//div//p[contains(text(),'" + questionsAndAnswers.get(questionElement.get(i).getText()) + "')]")));
-            assertEquals(questionsAndAnswers.get(questionElement.get(i).getText()), answerElement.get(i).getText());
-        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(homePage.getAnswerField()));
+        assertEquals(answer, answerField.getText());
     }
 }
